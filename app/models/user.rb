a3afb8d3baf40d0dc6e5593c159c795b9c7b739a
valzav <valentine.zavgorodnev@gmail.com>
2013-09-29 10:58:19 -0700

    -

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
                           price: miner.price,
                           ghps: miner.ghps,
                           power_use_watt: miner.power_use_watt,
-                          effective_date: Time.now + (7*24*60*60)
+                          effective_date: (Time.now + (7*24*60*60)).to_date
                         })
     self.market_env = MarketEnv.new(monthly_growth: 80, investment_horizon: 6)
   end
